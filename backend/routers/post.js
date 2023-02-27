f47649c5c19d3daca48066991ff4e8d559f97791
JRS296 <jonathansamuel296@gmail.com>
2023-02-27 21:11:32 +0530

    Commit #10.3 - Deployment-changing backend data - 27.02.2023 @21:11

--- backend/routers/post.js
+++ backend/routers/post.js
@@ -8,16 +8,16 @@ const { parseData } = require('../middleware/ParserForTagsFeatured');
 
 //CRUD Operations 
 //Create Route
-// router.post('/create',
-//  multer.single('thumbnail'),
-//  parseData,
-//  postValidator, 
-//  validate, 
-//  createPost);
+router.post('/create',
+ multer.single('thumbnail'),
+ parseData,
+ postValidator, 
+ validate, 
+ createPost);
 
-//  router.post('/upload-image', //For Image upload in Admin Dashboard
-//  multer.single('image'),
-//  uploadImage);
+ router.post('/upload-image', //For Image upload in Admin Dashboard
+ multer.single('image'),
+ uploadImage);
 
 //Read Route(s)
 router.get('/single/:slug', getPost); //Changed from /single/:postId to :slug
@@ -27,15 +27,15 @@ router.get('/search', searchPost);
 router.get('/related-posts/:postId', getRelatedPosts);
 
 //Update Route
-// router.put('/:postId',
-//  multer.single('thumbnail'),
-//  parseData,
-//  postValidator, 
-//  validate, 
-//  updatePost
-//  );
+router.put('/:postId',
+ multer.single('thumbnail'),
+ parseData,
+ postValidator, 
+ validate, 
+ updatePost
+ );
 
 //Delete Route
-// router.delete('/:postId', deletePost)
+router.delete('/:postId', deletePost)
 
 module.exports = router;
