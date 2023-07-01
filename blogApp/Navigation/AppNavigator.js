9be98457aa0ce112ad84b7160473c75832f18452
JRS296 <jonathansamuel296@gmail.com>
2023-07-01 23:58:10 +0530

    minor changes - v2.01. - 01.07.2023 @23:58

--- blogApp/Navigation/AppNavigator.js
+++ blogApp/Navigation/AppNavigator.js
@@ -5,7 +5,7 @@ import Home from '../components/Screens/Home';
 import PostDetail from '../components/PostDetail';
 import { Ionicons } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
-
+import FocusAwareStatusBar from '../components/FocusAwareStatusBar'
 
 const Stack = createNativeStackNavigator();
 
@@ -13,27 +13,30 @@ export default function AppNavigator() {
     const navigation = useNavigation();
 
     return (
-        <Stack.Navigator>
-            <Stack.Screen options={{ headerShown: false, }} component={Home} name="Home" />
-            <Stack.Screen options={{
-                title: "",
-                headerTransparent: true,
-                headerShadowVisible: false,
-                headerLeft: (props) => (
-                    <TouchableWithoutFeedback {...props} onPress={navigation.goBack}>
-                        <View style={{
-                            width: 40,
-                            height: 40,
-                            justifyContent: 'center',
-                            alignItems: 'center',
-                            borderRadius: 20,
-                            backgroundColor: "rgba(0,0,0,0.2)",
-                        }}>
-                            <Ionicons name='arrow-back' size={24} color="white" />
-                        </View>
-                    </TouchableWithoutFeedback>
-                )
-            }} component={PostDetail} name="PostDetail" />
-        </Stack.Navigator>
+        <>
+            <FocusAwareStatusBar backgroundColor="rgba(255,255,255,1)" barStyle="dark-content" />
+            <Stack.Navigator>
+                <Stack.Screen options={{ headerShown: false, }} component={Home} name="Home" />
+                <Stack.Screen options={{
+                    title: "",
+                    headerTransparent: true,
+                    headerShadowVisible: false,
+                    headerLeft: (props) => (
+                        <TouchableWithoutFeedback {...props} onPress={navigation.goBack}>
+                            <View style={{
+                                width: 40,
+                                height: 40,
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                                borderRadius: 20,
+                                backgroundColor: "rgba(0,0,0,0.2)",
+                            }}>
+                                <Ionicons name='arrow-back' size={24} color="white" />
+                            </View>
+                        </TouchableWithoutFeedback>
+                    )
+                }} component={PostDetail} name="PostDetail" />
+            </Stack.Navigator>
+        </>
     )
 };
